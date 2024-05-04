550fe81ea60b6321ac60016dfa5b62a4be436469
abedsully <abedsully2909@gmail.com>
2024-05-04 19:29:59 +0700

    Initial Commit

---
+++ IShowShop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IShowShop
+//
+//  Created by Stefanus Albert Wilson on 5/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IShowShop/IShowShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  IShowShopApp.swift
+//  IShowShop
+//
+//  Created by Stefanus Albert Wilson on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct IShowShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
