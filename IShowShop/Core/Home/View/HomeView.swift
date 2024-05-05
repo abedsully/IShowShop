f0e0261cf8a6870234d7fb1e810945f243f2ce40
abedsully <abedsully2909@gmail.com>
2024-05-05 11:21:59 +0700

    fix: bug tab bar view

--- IShowShop/Core/Home/View/HomeView.swift
+++ IShowShop/Core/Home/View/HomeView.swift
@@ -8,70 +8,75 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var selectedCategory: CategoryFilter = .clothes
     
     var body: some View {
-        SearchBarView()
-        
-        WalletView()
+        ScrollView {
+            SearchBarView()
+            
+            WalletView()
+                .padding(.horizontal, 10)
+            
+            // Most Purchased Items
+            VStack (alignment: .leading){
+                Text("Most Purchased Items")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                ScrollView(.horizontal) {
+                    HStack (spacing: 15){
+                        ForEach(0 ... 10, id: \.self) { items in
+                            VStack {
+                                Image("category")
+                                    .resizable()
+                                    .frame(width: 100, height: 100)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                                
+                                Text("Denim Jacket")
+                                    .font(.footnote)
+                            }
+                        }
+                    }
+                }
+                .scrollIndicators(.hidden)
+            }
             .padding(.horizontal, 10)
-        
-        // Most Purchased Items
-        VStack (alignment: .leading){
-            Text("Most Purchased Items")
-                .font(.title2)
-                .fontWeight(.semibold)
             
-            ScrollView(.horizontal) {
-                HStack (spacing: 15){
-                    ForEach(0 ... 10, id: \.self) { items in
-                        VStack {
-                            Image("category")
-                                .resizable()
-                                .frame(width: 100, height: 100)
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                            
-                            Text("Denim Jacket")
-                                .font(.footnote)
+            
+            // Promo
+            PromoView()
+                .padding(.vertical)
+            
+            // Highest Rated Items
+            VStack (alignment: .leading){
+                Text("Highest Rated Items")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                ScrollView(.horizontal) {
+                    HStack (spacing: 15){
+                        ForEach(0 ... 10, id: \.self) { items in
+                            VStack {
+                                Image("category")
+                                    .resizable()
+                                    .frame(width: 100, height: 100)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                                
+                                Text("Denim Jacket")
+                                    .font(.footnote)
+                            }
                         }
                     }
                 }
+                .scrollIndicators(.hidden)
             }
-            .scrollIndicators(.hidden)
-        }
-        .padding(.horizontal, 10)
-        
-        
-        // Promo
-        PromoView()
-        
-        // Highest Rated Items
-        VStack (alignment: .leading){
-            Text("Highest Rated Items")
-                .font(.title2)
-                .fontWeight(.semibold)
+            .padding(.horizontal, 10)
             
-            ScrollView(.horizontal) {
-                HStack (spacing: 15){
-                    ForEach(0 ... 10, id: \.self) { items in
-                        VStack {
-                            Image("category")
-                                .resizable()
-                                .frame(width: 100, height: 100)
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                            
-                            Text("Denim Jacket")
-                                .font(.footnote)
-                        }
-                    }
-                }
-            }
-            .scrollIndicators(.hidden)
         }
-        .padding(.horizontal, 10)
-
+        .scrollIndicators(.hidden)
+        .padding(.top)
     }
 }
+
 
 #Preview {
     HomeView()
